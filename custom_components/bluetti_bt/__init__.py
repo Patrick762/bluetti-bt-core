b91868562a6ef571c25e12a18dc18f238845346b
Patrick762 <lutzpatti86@gmail.com>
2026-09-19 23:35:17 +0200

    prepare reconfigure and unload flow

--- custom_components/bluetti_bt/__init__.py
+++ custom_components/bluetti_bt/__init__.py
@@ -17,6 +17,14 @@ type BluettiBtConfigEntry = ConfigEntry[dict[str, str]]
 async def async_setup_entry(hass: HomeAssistant, entry: BluettiBtConfigEntry) -> bool:
     """Set up Bluetti BT from a config entry."""
 
-    entry.runtime_data.setdefault({})
+    #entry.runtime_data.setdefault({})
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: BluettiBtConfigEntry) -> bool:
+    """Unload Bluetti BT config entry."""
+
+    # TODO stop coordinator
 
     return True
--- custom_components/bluetti_bt/config_flow.py
+++ custom_components/bluetti_bt/config_flow.py
@@ -1,11 +1,11 @@
 import logging
-import json
+from typing import Any
+from probatio import Schema
 
 from habluetooth import BluetoothServiceInfoBleak
 from homeassistant import config_entries
 from homeassistant.const import CONF_ADDRESS, CONF_MODEL, CONF_API_VERSION
 from homeassistant.data_entry_flow import FlowResult
-import voluptuous as vol
 
 from .const import DOMAIN, CONF_ENCRYPTION
 
@@ -20,9 +20,7 @@ class BluettiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._discovery_info: BluetoothServiceInfoBleak | None = None
         self._discovered_devices: dict[str, BluetoothServiceInfoBleak] = {}
 
-    async def async_step_bluetooth(
-        self, discovery_info: BluetoothServiceInfoBleak
-    ) -> FlowResult:
+    async def async_step_bluetooth(self, discovery_info: BluetoothServiceInfoBleak) -> FlowResult:
         """Handle bluetooth discovery."""
         _LOGGER.debug(f"Discovered matching device {discovery_info.name}")
         await self.async_set_unique_id(discovery_info.address)
@@ -31,9 +29,7 @@ class BluettiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self.context["title_placeholders"] = {"name": discovery_info.name}
         return await self.async_step_user()
 
-    async def async_step_user(
-        self, user_input: dict[str, any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle user input."""
 
         # Handle discovery proceed setup
@@ -43,18 +39,12 @@ class BluettiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
             self._abort_if_unique_id_configured()
 
-            # Run model detection
-            # TODO
+            data = self._detect_bluetti_device()
 
             # Save entry
             return self.async_create_entry(
                 title=self._discovery_info.name,
-                data={
-                    CONF_ADDRESS: self._discovery_info.address,
-                    CONF_MODEL: "Dummy",
-                    CONF_API_VERSION: 1,
-                    CONF_ENCRYPTION: False,
-                },
+                data=data,
             )
 
         if not self._discovery_info:
@@ -63,5 +53,36 @@ class BluettiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # We don't have manual configs, only via discovery
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({}),
+            data_schema=Schema({}),
         )
+
+    async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None) -> FlowResult:
+        if user_input is not None:
+            await self.async_set_unique_id(
+                self._discovery_info.address, raise_on_progress=False
+            )
+            self._abort_if_unique_id_mismatch()
+
+            data = self._detect_bluetti_device()
+
+            return self.async_update_reload_and_abort(
+                self._get_reconfigure_entry(),
+                data_updates=data,
+            )
+
+        # We don't have manual configs, only via discovery
+        return self.async_show_form(
+            step_id="reconfigure",
+            data_schema=Schema({}),
+        )
+
+    def _detect_bluetti_device(self) -> dict:
+        # Run model detection
+        # TODO
+
+        return {
+            CONF_ADDRESS: self._discovery_info.address,
+            CONF_MODEL: "Dummy",
+            CONF_API_VERSION: 1,
+            CONF_ENCRYPTION: False,
+        }
